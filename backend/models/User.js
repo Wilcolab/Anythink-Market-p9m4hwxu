1cf3d310a27622898a67d1f8a7c7d9420f6697e8
NivRichter <nivrichter@gmail.com>
2023-09-06 11:48:14 +0000

    DSfdsf

--- backend/models/User.js
+++ backend/models/User.js
@@ -89,6 +89,8 @@ UserSchema.methods.toProfileJSONFor = function(user) {
   };
 };
 
+
+
 UserSchema.methods.favorite = function(id) {
   if (this.favorites.indexOf(id) === -1) {
     this.favorites = this.favorites.concat([id]);
